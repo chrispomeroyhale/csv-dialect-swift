89b3c860f0c7a2f84e2660da5d2013f0989ed70a
slythfox <slythfox@users.noreply.github.com>
2020-05-09 12:11:33 -0700

    Improved test coverage for export.

--- Tests/DialectalCSVTests/ExportTests.swift
+++ Tests/DialectalCSVTests/ExportTests.swift
@@ -4,23 +4,58 @@ import XCTest
 class ExportTests : XCTestCase {
 
     static var allTests = [
+        ("testEscapeCharacter", testEscapeCharacter),
+        ("testNoRows", testNoRows),
         ("testNullSequenceValues", testNullSequenceValues),
     ]
 
+    func testEscapeCharacter() {
+        let rows = [
+            ["\"quote\"", "\"author\""],
+            ["The best way to find yourself is to lose yourself in the ,service, of others.", "'" + Authors.mahatmaGandhi.rawValue],
+            ["Always bear in mind that your own resolution to succeed is more important than any 'other.", Authors.abrahamLincoln.rawValue],
+        ]
+        var dialect = Dialect()
+        dialect.escapeCharacter = "'"
+        let document = Document(allRows: rows, dialect: dialect)
+        guard let data = try? document.export(dialect: dialect) else {
+            XCTFail()
+            return
+        }
+        let expected = Utility.fixture(named: "escapeCharacter.csv")
+        XCTAssertEqual(String(data: data, encoding: .utf8), String(data: expected, encoding: .utf8))
+    }
+
+    func testNoRows() {
+        let rows = [Row]()
+        var dialect = Dialect()
+        dialect.header = false
+        let document = Document(allRows: rows, dialect: dialect)
+        guard let data = try? document.export(dialect: dialect) else {
+            XCTFail()
+            return
+        }
+        XCTAssertTrue(data.isEmpty)
+    }
+
     func testNullSequenceValues() {
-        let records = [
+        let rows = [
             ["quote", "author"],
             [nil, "null"],
             [Quotes.abrahamLincoln.rawValue, "   null"],
         ]
         var dialect = Dialect()
         dialect.nullSequence = "null"
-        let document = Document(header: nil, records: records, dialect: dialect)
+        let document = Document(allRows: rows, dialect: dialect)
         guard let data = try? document.export(dialect: dialect) else {
             XCTFail()
             return
         }
-        XCTAssertEqual(String(data: data, encoding: .utf8), String(data: Utility.fixture(named: "nullSequenceValues.csv"), encoding: .utf8))
+        let expected = Utility.fixture(named: "nullSequenceValues.csv")
+        XCTAssertEqual(String(data: data, encoding: .utf8), String(data: expected, encoding: .utf8))
+
+        dialect.nullSequence = nil
+        XCTAssertThrowsError(try document.export(dialect: dialect))
     }
 
 }
--- Tests/DialectalCSVTests/ImportTests.swift
+++ Tests/DialectalCSVTests/ImportTests.swift
@@ -27,7 +27,6 @@ class ImportTests: XCTestCase {
         let data = Utility.fixture(named: "escapeCharacter.csv")
         var dialect = Dialect()
         dialect.escapeCharacter = "'"
-        dialect.doubleQuote = false
         let document = try! Document(data: data, dialect: dialect)
 
         XCTAssertEqual(document.header!.count, 2)
@@ -36,7 +35,7 @@ class ImportTests: XCTestCase {
         XCTAssertEqual(document.records[0][0], "The best way to find yourself is to lose yourself in the ,service, of others.")
         XCTAssertEqual(document.records[0][1], "'" + Authors.mahatmaGandhi.rawValue)
 
-        // Regression: Ensure that escape status 
+        // Regression: Ensure that escape status
         XCTAssertEqual(document.records[1][0], "Always bear in mind that your own resolution to succeed is more important than any 'other.")
         XCTAssertEqual(document.records[1][1], Authors.abrahamLincoln.rawValue)
     }
